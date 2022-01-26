2ca588b392787e8b81d0978cd24e9f703969d5d2
OmerTurgut55 <omerturgut72@gmail.com>
2022-01-27 00:16:44 +0300

    first commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            string connectionString = "Data Source=(LocalDb)\\MSSQLLocalDB; Initial Catalog=Database; Integrated Security=SSPI;";
+
+            builder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Database.Models
+{
+    public class Post
+    {
+        // Entity Model Tanımları
+        // Kolonu Primary Key olarak ayarlar.
+        [Key]
+        [Required]
+        // Kolonu Auto Increment olarak ayarlar.
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(400)]
+        [Column(TypeName = "nvarchar(400)")]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string CreatedBy { get; set; }
+    }
+}
